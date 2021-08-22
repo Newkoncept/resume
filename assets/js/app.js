d26064f5c0c044ba678d786aa5c82e5075bf1e4f
Taiwo Oluwagbemiga <49463561+Newkoncept@users.noreply.github.com>
2021-08-22 03:44:25 +0100

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,45 @@
 // variables
 const topBtn = document.querySelector(".topBtn i");
 const hamburger = document.querySelector(".hamburger i");
+const about = document.querySelector("#about");
 
 // Event Listeners
 document.addEventListener("scroll", displayBtn);
 document.addEventListener("scroll", navBarDisplay);
+about.addEventListener("mousein", displayStyle);
 hamburger.addEventListener("click", displayNav);
 
 // Functions
 function displayBtn() {
-  let scrollHeight = document.documentElement.scrollTop;
-  if (scrollHeight >= 100) {
-    topBtn.style.display = "flex";
-    topBtn.style.transition = "2s";
-  } else {
-    topBtn.style.display = "none";
-    topBtn.style.transition = "2s";
-  }
-}
-
-function displayNav(e) {
-  e.preventDefault();
-  const navDisplay = document.querySelector(".nav-display");
-  if (navDisplay.classList.contains("close")) {
-    navDisplay.style.display = "flex";
-    navDisplay.classList.remove("close");
-    navDisplay.classList.add("open");
-    hamburger.classList.remove("fa-bars");
-    hamburger.classList.add("fa-close");
-  } else {
-    navDisplay.style.display = "none";
-    hamburger.style.right = "30px";
-    navDisplay.classList.add("close");
-    navDisplay.classList.remove("open");
-    hamburger.classList.add("fa-bars");
-    hamburger.classList.remove("fa-close");
-  }
+	let scrollHeight = document.documentElement.scrollTop;
+	if (scrollHeight >= 100) {
+		topBtn.style.display = "flex";
+		topBtn.style.transition = "2s";
+	} else {
+		topBtn.style.display = "none";
+		topBtn.style.transition = "2s";
+	}
 }
 
 function navBarDisplay() {
-  let scrollHeight = document.documentElement.scrollTop;
-  const nav = document.querySelector("nav");
+	let scrollHeight = document.documentElement.scrollTop;
+	const nav = document.querySelector("nav");
 
-  if (scrollHeight >= 400) {
-    nav.style.background = "green";
-  } else {
-    nav.style.background = "transparent";
-  }
+	if (scrollHeight >= 400) {
+		nav.style.background = "green";
+	} else {
+		nav.style.background = "transparent";
+	}
+}
+
+function displayNav(e) {
+	e.preventDefault();
+	const navDisplay = document.querySelector(".nav-display");
+	navDisplay.classList.toggle("open");
+	hamburger.classList.toggle("fa-close");
+	hamburger.classList.toggle("fa-bars");
+}
+
+function displayStyle() {
+	about.classList.add("animate__animated animate__pulse");
 }
